b10f88f2a28d9dcd17729718222844afbd37adcc
Ayushi Mate <88182566+Ayushimate@users.noreply.github.com>
2022-01-27 19:40:03 +0530

    Add files via upload

---
+++ Assignment day 1.js
@@ -0,0 +1,5 @@
+let r = prompt('Enter radius');
+let area = (22/7)*r*r;
+let circumference = 2*(22/7)*r;
+console.log('The Area for Radius = ', r, 'is', area);
+console.log('The Circumference for Radius = ', r, 'is', circumference);
