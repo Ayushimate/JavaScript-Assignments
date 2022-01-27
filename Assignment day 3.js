e1b1303054803ed0dd7df8badec2555da5ebd2d2
Ayushi Mate <88182566+Ayushimate@users.noreply.github.com>
2022-01-27 19:59:25 +0530

    Add files via upload

---
+++ Assignment day 3.js
@@ -0,0 +1,9 @@
+let email = prompt('Enter Your Email');
+let pass = prompt('Enter the password');
+if(email == 'stu@letsupgrade.com' && pass == '1234')
+{
+    alert('Logged in!');
+}
+else {
+    alert('Wrong email or password!');
+}
